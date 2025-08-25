c98a61293d75d16eb54a922b0af390ad870e38f7
FelipeDiRisioIze <fdirisioize@mail.austral.edu.ar>
2025-08-25 11:16:50 -0700

    change.py

--- change.py
+++ change.py
@@ -1,3 +1,14 @@
 def change():
     expense = 23.75
     money = 100
+    change = money - expense
+
+pesos = int(change)
+centavos = int((change * 100) - (pesos * 100))
+
+print("\nVuelto\n")
+print("Pesos:")
+print(pesos)
+print("Centavos")
+print(centavos)
+
