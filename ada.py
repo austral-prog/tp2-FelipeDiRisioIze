714ec56c3547508a6d4712b614a4679c96863bf6
FelipeDiRisioIze <fdirisioize@mail.austral.edu.ar>
2025-08-25 11:31:12 -0700

    ada.py

--- ada.py
+++ ada.py
@@ -3,7 +3,7 @@ def ada():
     last_name = "LoVeLAce"
     name = f"{first_name} {last_name}"
 
-print(name.lower())
-print(name.title())
-print(name.upper())
-print("\t" + name.lower())
+    print(name.lower())
+    print(name.title())
+    print(name.upper())
+    print("\t" + name.lower())
