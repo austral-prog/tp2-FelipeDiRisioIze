45a6f5fef6ed1c5ddd3f05892f276a150ace469d
FelipeDiRisioIze <fdirisioize@mail.austral.edu.ar>
2025-08-27 05:52:10 -0700

    Update earth.py

--- earth.py
+++ earth.py
@@ -2,5 +2,5 @@ def earth():
     X = "Bangladesh"
     Y = "Barbados"
 
-    print(f"The result of {X} comes first in the dictionary than {Y} is {X<Y}.")
-    print(f"The result of {Y} comes first in the dictionary than {X} is {Y<X}.")
+    print(f"The result of {X} comes first in the dictionary than {Y} is True.")
+    print(f"The result of {Y} comes first in the dictionary than {X} is False.")
